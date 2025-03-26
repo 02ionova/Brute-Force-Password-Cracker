721c207be9430aaf9a054725e57fb3ae063408ed
natalia <02ionova@gmail.com>
2025-03-26 18:00:54 +0100

    Initial commit

---
+++ src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
@@ -0,0 +1,26 @@
+package cz.cvut.fel.pjv;
+
+public class BruteForceAttacker extends Thief {
+
+    @Override
+    public void breakPassword(int sizeOfPassword) {
+        char[] characters = getCharacters();
+        char[] attempt = new char[sizeOfPassword];
+        bruteForce(attempt, characters, 0);
+    }
+
+    private boolean bruteForce(char[] attempt, char[] characters, int position) {
+        if (position == attempt.length) {
+            return tryOpen(attempt);
+        }
+
+        for (char c : characters) {
+            attempt[position] = c;
+            if (bruteForce(attempt, characters, position + 1)) {
+                return true; // Если нашли правильный пароль, прекращаем перебор
+            }
+        }
+
+        return false;
+    }
+}
